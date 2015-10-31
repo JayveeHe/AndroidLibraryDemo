3e193a9ee0be7272b1dd7efa4e972b12761d4c5a
zishell <zisheer@gmail.com>
2015-10-31 16:10:55 +0800

    init commit

--- app/src/main/java/io/petchat/example/androidlibrarydemo/MainActivity.java
+++ app/src/main/java/io/petchat/example/androidlibrarydemo/MainActivity.java
@@ -2,6 +2,9 @@ package io.petchat.example.androidlibrarydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.petchat.example.mylibrary.MyMath;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(this.getClass().toString(), "==>" + MyMath.add(1, 1));
+
     }
 }
