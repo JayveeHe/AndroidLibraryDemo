4cd172643587aa200e55de50ba2247e67463e97b
zhangzishell <zishell@zishells-mac.local>
2015-10-31 14:10:18 +0800

    init commit

---
+++ mylibrary/src/main/java/io/petchat/example/mylibrary/MyMath.java
@@ -0,0 +1,10 @@
+package io.petchat.example.mylibrary;
+
+/**
+ * Created by zishell on 10/31/15.
+ */
+public class MyMath {
+    public static int add(final int a, final int b) {
+        return a + b;
+    }
+}
